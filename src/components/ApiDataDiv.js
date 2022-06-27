c4d5366f020c854cb70f450f5f4ea87e9dd18a09
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-27 12:01:30 -0400

    Update the error from ApiDataDiv.js

--- src/components/ApiDataDiv.js
+++ src/components/ApiDataDiv.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const ApiDataDiv = (props) => {
-  return <div style={{ ...props.style }}>Data</div>;
+  return <div style={{ ...props.style }}>ApiDataDiv</div>;
 };
