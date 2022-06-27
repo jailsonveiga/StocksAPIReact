306cd0e5e8bb82889de5198324e7372e0fb1a1f1
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-27 15:47:54 -0400

    Update ApiDataDiv.js

--- src/components/ApiDataDiv.js
+++ src/components/ApiDataDiv.js
@@ -1,5 +1,35 @@
 import React from "react";
+import { useApiDataContext } from "../providers/ApiDataProvider";
 
 export const ApiDataDiv = (props) => {
-  return <div style={{ ...props.style }}>ApiDataDiv</div>;
+  const { data } = useApiDataContext();
+
+  return (
+    <div style={{ ...props.style }}>
+      <h2>Data</h2>
+      {/* {JSON.stringify(data, null, 2)} */}
+      {data.length > 0
+        ? data.map((stock) => {
+            return (
+              <div
+                key={stock.id}
+                style={{
+                  border: "1px solid grey",
+                  marginTop: 10,
+                  borderRadius: 5,
+                }}
+              >
+                <h3>
+                  {stock.Symbol} - {stock.Exchange}
+                </h3>
+                <p>{stock.Name}</p>
+                <p>
+                  Market Cap: ${stock.MarketCapitalization.toLocaleString("us")}
+                </p>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
 };
