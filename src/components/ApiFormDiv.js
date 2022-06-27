a569c3efa6fde07badf8f2bcbb01e06ec87d3d64
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-27 15:49:04 -0400

    Update ApiFormDiv.js

--- src/components/ApiFormDiv.js
+++ src/components/ApiFormDiv.js
@@ -1,20 +1,32 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useApiDataContext } from "../providers/ApiDataProvider";
 
 export const ApiFormDiv = (props) => {
   const BASE_URI = "http://localhost:8080/api/overview/";
 
   const [inputData, setInputData] = useState("");
 
+  const [selected, setSelected] = useState("all");
+
+  const { setData } = useApiDataContext();
+
+  const noQueryOptions = ["all"];
+
   const requestData = () => {
     console.log("Requesting Data...");
     console.log(inputData);
 
     axios
-      .get(BASE_URI + "all")
+      .get(
+        BASE_URI +
+          (noQueryOptions.includes(selected)
+            ? selected
+            : selected + "/" + inputData)
+      )
       .then((res) => {
+        setData(res.data);
         console.log(res.data);
-        alert("Request was successful");
       })
       .catch((err) => {
         alert("Error: \n" + err.message);
@@ -25,8 +37,26 @@ export const ApiFormDiv = (props) => {
   return (
     <div style={{ ...props.style }}>
       <h2>API Form</h2>
+
+      <select
+        value={selected}
+        onChange={(e) => {
+          setSelected(e.target.value);
+        }}
+      >
+        <option value="all">All</option>
+        <option value="exchange">By Exchange</option>
+        <option value="symbol">By Symbol</option>
+        <option value="marketcapgte">Market Cap Greater Than</option>
+        <option value="yearhighgte">Year High Greater Then</option>
+        <option value="ddbefore">Dividend Data Before yyyy-mm-dd</option>
+      </select>
+
       <input
-        style={{ textAlign: "center" }}
+        style={{
+          textAlign: "center",
+          display: noQueryOptions.includes(selected) ? "none" : "initial",
+        }}
         type="text"
         id="text-input"
         placeholder="Search Data"
